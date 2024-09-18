dc2dae065b3ae1ce38e121a09e6926c1aaae2ab0
Wanjiku-Gichia <77983272+Wanjiku-Gichia@users.noreply.github.com>
2024-09-18 15:33:04 +0300

    added axios to my html document,added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updatedTemperature(response) {
+  let temperature = document.querySelector("#temperature");
+  temperatureValue = response.data.temperature.current;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.city;
+  temperature.innerHTML = Math.round(temperatureValue);
+}
+function search(city) {
+  let key = "f1d4abdc3aeo0d6af4ddd53f42t144ba";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+  axios.get(url).then(updatedTemperature);
+}
+
 function handleSearch(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-city");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = city.value;
+  let cityInput = document.querySelector("#search-city");
+
+  search(cityInput.value);
 }
 let searchCity = document.querySelector("#search-form");
 searchCity.addEventListener("submit", handleSearch);
+search("Nairobi");
